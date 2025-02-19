7cdff8eb7dea0eea7be7017a4ac2bc9451628e82
Greg T. Wallace <greg@gregtwallace.com>
2025-02-19 18:07:16 -0500

    add delete script

---
+++ dev_delete.sh
@@ -0,0 +1,5 @@
+# remove all CW Client folders (this is for dev use, not for removing a properly installed plugin)
+
+rm -rf /usr/local/opnsense/mvc/app/views/GregTWallace/CertWardenClient
+rm -rf /usr/local/opnsense/mvc/app/models/GregTWallace/CertWardenClient
+rm -rf /usr/local/opnsense/mvc/app/controllers/GregTWallace/CertWardenClient
