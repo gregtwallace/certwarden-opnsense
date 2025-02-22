209bd588217eebd1975e0f724f95219933f26a43
Greg T. Wallace <greg@gregtwallace.com>
2025-02-22 16:48:22 -0500

    dev: make setup env a little more robust

--- dev/setup_dev_env.py
+++ dev/setup_dev_env.py
@@ -13,22 +13,21 @@ script_dir = dirname = os.path.dirname(__file__)
 # paths relevant to this script
 base_dir = os.path.join(script_dir, "../")
 core_dir = os.path.join(base_dir, "./core")
-core_git_dir = os.path.join(core_dir, "./.git")
 
 try:
   # confirm core dir exists as expected
   coreExists = os.path.exists(core_dir)
   if not coreExists:
     raise Exception("core dir is missing")
-
-  coreGitExists = os.path.exists(core_git_dir)
-  if not coreGitExists:
-    raise Exception("core git dir is missing")
+  
+  # confirm the top level git path is the expected one
+  gitDir = subprocess.check_output(["git", "rev-parse", "--show-toplevel"], cwd=core_dir).strip().decode('ascii')
+  if (os.path.realpath(core_dir, strict=True) != os.path.realpath(gitDir, strict=True)):
+    raise Exception("core dir git project not found")
 
   # if, so try to git pull
-  process = subprocess.Popen(["git", "pull"], cwd=core_dir, stdout=subprocess.PIPE)
-  output = process.communicate()[0]
-  print(output)
+  pullOutput = subprocess.check_output(["git", "pull"], cwd=core_dir).strip().decode('ascii')
+  print(pullOutput)
 
 except:
   redoCore = input("Error encountered with core or git. Delete ./core path and re clone (y/N)? ")
@@ -39,6 +38,5 @@ except:
   shutil.rmtree(core_dir)
 
   # clone it again
-  process = subprocess.Popen(["git", "clone", "https://github.com/opnsense/core"], cwd=base_dir, stdout=subprocess.PIPE)
-  output = process.communicate()[0]
-  print(output)
+  cloneOutput = subprocess.check_output(["git", "clone", "https://github.com/opnsense/core"], cwd=base_dir).strip().decode('ascii')
+  print(cloneOutput)
